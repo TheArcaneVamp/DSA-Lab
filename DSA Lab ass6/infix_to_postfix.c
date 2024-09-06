d961d5d6f1e51ca896328ccce0a640791f55369e
TheArcaneVamp <146957043+TheArcaneVamp@users.noreply.github.com>
2024-09-06 17:11:03 +0530

    Add files via upload

---
+++ DSA Lab ass6/infix_to_postfix.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char stack[100];
+char Q[100];
+char P[100];
+
+int topP=-1;
+int top=-1;
+
+void push(char e)
+{
+	top++;
+	stack[top] = e;	
+}
+
+
+void PushingP(char e)
+{
+	topP ++;
+	P[topP]= e;
+}
+
+
+void pop()
+{
+	PushingP(stack[top]);
+	top--;	
+}
+
+
+int isoperator(char e)
+{
+	if( e == '+' || e == '-' )
+	{
+		return 1;
+	}
+	else if( e == '*' || e == '/')
+	{
+		return 2;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
+void main()
+{
+	int i = 0;
+	int x;
+	stack[0] = '(';
+	
+	printf("Enter the infix expression:");
+	
+	while(1)
+	{
+		Q[i] = getchar();
+		if (Q[i] == '\n')
+		{
+			break;
+		}
+		i++;
+	}
+	
+	Q[i] = ')';
+	Q[i+1] = '\0';
+	for(i = 0; i< strlen(Q)-1; i++ )
+	{
+		if (isdigit(Q[i]))
+		{
+			PushingP(Q[i]);
+		}
+		else if (Q[i]== '(')
+		{
+			push(Q[i]);
+		}
+		else if(isoperator(Q[i]))
+		{
+			x = isoperator(Q[i]);
+			while(x<=isoperator(stack[top]))
+			{
+				pop();	
+			}
+			
+			push(Q[i]);	
+		}
+	
+		else if( Q[i] == ')')
+		{
+			while(stack[top] != '(')
+			{
+				pop();
+			}
+			top--; //poping the stack
+		}
+	}
+	
+	while(top != -1)
+	{
+		pop();
+	}
+	
+	P[topP+1] = 
+	'\0';
+	
+	printf("The Postfix expression is: %s\n", P);
+		
+}
